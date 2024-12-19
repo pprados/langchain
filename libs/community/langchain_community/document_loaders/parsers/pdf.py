2d63d62ed264bf824bec08a2401d0682dd6a94d6
Philippe Prados <github@prados.fr>
2024-12-19 12:18:36 +0100

    Fix imports tests and nb

--- libs/community/langchain_community/document_loaders/parsers/pdf.py
+++ libs/community/langchain_community/document_loaders/parsers/pdf.py
@@ -28,6 +28,8 @@ from urllib.parse import urlparse
 
 import numpy as np
 from langchain.prompts import Prompt
+from langchain_community.document_loaders.base import BaseBlobParser
+from langchain_community.document_loaders.blob_loaders import Blob
 from langchain_core._api.deprecation import (
     deprecated,
 )
@@ -35,9 +37,6 @@ from langchain_core.documents import Document
 from langchain_core.language_models import BaseChatModel
 from langchain_core.messages import HumanMessage
 from langchain_core.prompts import BasePromptTemplate, PromptTemplate
-
-from langchain_community.document_loaders.base import BaseBlobParser
-from langchain_community.document_loaders.blob_loaders import Blob
 
 if TYPE_CHECKING:
     import pdfplumber
@@ -2374,7 +2373,9 @@ class AmazonTextractPDFParser(BaseBlobParser):
         the blob.data is taken
         """
 
-        url_parse_result = urlparse(str(blob.path)) if blob.path else None  # type: ignore[attr-defined]
+        url_parse_result = (
+            urlparse(str(blob.path)) if blob.path else None
+        )  # type: ignore[attr-defined]
         # Either call with S3 path (multi-page) or with bytes (single-page)
         if (
             url_parse_result
@@ -2431,7 +2432,9 @@ class DocumentIntelligenceParser(BaseBlobParser):
         self.client = client
         self.model = model
 
-    def _generate_docs(self, blob: Blob, result: Any) -> Iterator[Document]:  # type: ignore[valid-type]
+    def _generate_docs(
+        self, blob: Blob, result: Any
+    ) -> Iterator[Document]:  # type: ignore[valid-type]
         for p in result.pages:
             content = " ".join([line.content for line in p.lines])
 
