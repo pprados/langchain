5836d4e4dd62096a5dbb7d136574607147c11d4e
Philippe Prados <github@prados.fr>
2025-01-02 17:58:36 +0100

    Update PDFMiner
    Update PyMuPDFParser
    
    Update PyPDF
    
    Add file_path with PurePath
    
    Update PyMuPDF
    
    Prepare the integration of new versions of PDFLoader.
    Add file_path with PurePath
    Add CloudBlobLoader in __init__
    Replace Dict/List to dict/list

--- libs/community/langchain_community/document_loaders/pdf.py
+++ libs/community/langchain_community/document_loaders/pdf.py
@@ -449,64 +449,144 @@ class PyPDFDirectoryLoader(BaseLoader):
 
 
 class PDFMinerLoader(BasePDFLoader):
-    """Load `PDF` files using `PDFMiner`."""
+    """Load and parse a PDF file using 'pdfminer.six' library.
+
+    This class provides methods to load and parse PDF documents, supporting various
+    configurations such as handling password-protected files, extracting images, and
+    defining extraction mode. It integrates the `pdfminer.six` library for PDF
+    processing and offers both synchronous and asynchronous document loading.
+
+    Examples:
+        Setup:
+
+        .. code-block:: bash
+
+            pip install -U langchain-community pdfminer.six
+
+        Instantiate the loader:
+
+        .. code-block:: python
+
+            from langchain_community.document_loaders import PDFMinerLoader
+
+            loader = PDFMinerLoader(
+                file_path = "./example_data/layout-parser-paper.pdf",
+                # headers = None
+                # password = None,
+                mode = "single",
+                pages_delimitor = "\n\f",
+                # extract_images = True,
+                # images_to_text = convert_images_to_text_with_tesseract(),
+            )
+
+        Lazy load documents:
+
+        .. code-block:: python
+
+            docs = []
+            docs_lazy = loader.lazy_load()
+
+            for doc in docs_lazy:
+                docs.append(doc)
+            print(docs[0].page_content[:100])
+            print(docs[0].metadata)
+
+        Load documents asynchronously:
+
+        .. code-block:: python
+
+            docs = await loader.aload()
+            print(docs[0].page_content[:100])
+            print(docs[0].metadata)
+    """
 
     def __init__(
         self,
-        file_path: str,
+        file_path: Union[str, PurePath],
         *,
-        headers: Optional[dict] = None,
+        password: Optional[str] = None,
+        mode: Literal["single", "page"] = "single",
+        pages_delimitor: str = _default_page_delimitor,
         extract_images: bool = False,
-        concatenate_pages: bool = True,
+        images_to_text: CONVERT_IMAGE_TO_TEXT = None,
+        headers: Optional[dict] = None,
+        concatenate_pages: Optional[bool] = None,
     ) -> None:
-        """Initialize with file path.
+        """Initialize with a file path.
 
         Args:
-            extract_images: Whether to extract images from PDF.
-            concatenate_pages: If True, concatenate all PDF pages into one a single
-                               document. Otherwise, return one document per page.
-        """
-        try:
-            from pdfminer.high_level import extract_text  # noqa:F401
-        except ImportError:
-            raise ImportError(
-                "`pdfminer` package not found, please install it with "
-                "`pip install pdfminer.six`"
-            )
+            file_path: The path to the PDF file to be loaded.
+            headers: Optional headers to use for GET request to download a file from a
+              web path.
+            password: Optional password for opening encrypted PDFs.
+            mode: The extraction mode, either "single" for the entire document or "page"
+                for page-wise extraction.
+            pages_delimitor: A string delimiter to separate pages in single-mode
+                extraction.
+            extract_images: Whether to extract images from the PDF.
+            images_to_text: Optional function or callable to convert images to text
+                during extraction.
+            concatenate_pages: Deprecated. If True, concatenate all PDF pages into one
+                a single document. Otherwise, return one document per page.
 
+        Returns:
+            This method does not directly return data. Use the `load`, `lazy_load` or
+            `aload` methods to retrieve parsed documents with content and metadata.
+        """
         super().__init__(file_path, headers=headers)
         self.parser = PDFMinerParser(
-            extract_images=extract_images, concatenate_pages=concatenate_pages
+            password=password,
+            extract_images=extract_images,
+            images_to_text=images_to_text,
+            concatenate_pages=concatenate_pages,
+            mode=mode,
+            pages_delimitor=pages_delimitor,
         )
 
     def lazy_load(
         self,
     ) -> Iterator[Document]:
-        """Lazily load documents."""
+        """
+        Lazy load given path as pages.
+        Insert image, if possible, between two paragraphs.
+        In this way, a paragraph can be continued on the next page.
+        """
         if self.web_path:
-            blob = Blob.from_data(open(self.file_path, "rb").read(), path=self.web_path)  # type: ignore[attr-defined]
+            blob = Blob.from_data(  # type: ignore[attr-defined]
+                open(self.file_path, "rb").read(), path=self.web_path
+            )
         else:
             blob = Blob.from_path(self.file_path)  # type: ignore[attr-defined]
-        yield from self.parser.parse(blob)
+        yield from self.parser.lazy_parse(blob)
 
 
 class PDFMinerPDFasHTMLLoader(BasePDFLoader):
-    """Load `PDF` files as HTML content using `PDFMiner`."""
+    """Load `PDF` files as HTML content using `PDFMiner`.
+    Warning, the HTML output is just a positioning of the boxes,
+    without being able to interpret the HTML in an LLM.
+    """
 
-    def __init__(self, file_path: str, *, headers: Optional[dict] = None):
+    def __init__(
+        self,
+        file_path: Union[str, PurePath],
+        *,
+        password: Optional[str] = None,
+        headers: Optional[dict] = None,
+    ):
         """Initialize with a file path."""
+        super().__init__(file_path, headers=headers)
+        self.password = password
+
+    def lazy_load(self) -> Iterator[Document]:
+        """Load file."""
         try:
-            from pdfminer.high_level import extract_text_to_fp  # noqa:F401
+            from pdfminer.high_level import extract_text_to_fp
         except ImportError:
             raise ImportError(
                 "`pdfminer` package not found, please install it with "
                 "`pip install pdfminer.six`"
             )
 
-        super().__init__(file_path, headers=headers)
-
-    def lazy_load(self) -> Iterator[Document]:
-        """Load file."""
         from pdfminer.high_level import extract_text_to_fp
         from pdfminer.layout import LAParams
         from pdfminer.utils import open_filename
@@ -516,6 +596,7 @@ class PDFMinerPDFasHTMLLoader(BasePDFLoader):
             extract_text_to_fp(
                 cast(BinaryIO, fp),
                 output_string,
+                password=self.password or "",
                 codec="",
                 laparams=LAParams(),
                 output_type="html",
