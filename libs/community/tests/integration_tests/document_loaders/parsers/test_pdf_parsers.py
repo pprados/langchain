f93c8fee0246091e97aad5b7053400d7cdad5ab5
Philippe Prados <github@prados.fr>
2025-01-02 17:58:36 +0100

    Update PyPDF

--- libs/community/tests/integration_tests/document_loaders/parsers/test_pdf_parsers.py
+++ libs/community/tests/integration_tests/document_loaders/parsers/test_pdf_parsers.py
@@ -15,7 +15,6 @@ from langchain_community.document_loaders.parsers import (
     PDFMinerParser,
     PDFPlumberParser,
     PyPDFium2Parser,
-    PyPDFParser,
 )
 
 # PDFs to test parsers on.
@@ -95,11 +94,6 @@ def _assert_with_duplicate_parser(parser: BaseBlobParser, dedupe: bool = False) 
         assert "11000000 SSeerriieess" == docs[0].page_content.split("\n")[0]
 
 
-def test_pypdf_parser() -> None:
-    """Test PyPDF parser."""
-    _assert_with_parser(PyPDFParser())
-
-
 def test_pdfminer_parser() -> None:
     """Test PDFMiner parser."""
     # Does not follow defaults to split by page.
@@ -117,11 +111,6 @@ def test_pdfplumber_parser() -> None:
     _assert_with_parser(PDFPlumberParser())
     _assert_with_duplicate_parser(PDFPlumberParser())
     _assert_with_duplicate_parser(PDFPlumberParser(dedupe=True), dedupe=True)
-
-
-def test_extract_images_text_from_pdf_pypdfparser() -> None:
-    """Test extract image from pdf and recognize text with rapid ocr - PyPDFParser"""
-    _assert_with_parser(PyPDFParser(extract_images=True))
 
 
 def test_extract_images_text_from_pdf_pdfminerparser() -> None:
@@ -146,6 +135,8 @@ def test_extract_images_text_from_pdf_pypdfium2parser() -> None:
     "parser_factory,params",
     [
         ("PyMuPDFParser", {}),
+        ("PyPDFParser", {"extraction_mode": "plain"}),
+        ("PyPDFParser", {"extraction_mode": "layout"}),
     ],
 )
 def test_standard_parameters(
